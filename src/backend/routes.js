96892c31b919717333e1f4115cb5adfe4ca2d614
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2022-11-04 08:09:03 -0300

    ajeitando rotas

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -14,14 +14,15 @@ routes.get('/hello', helloController.index)
 routes.post('/session', sessionsController.create)
 routes.post('/users', userController.create)
 
+routes.get('/iniciatives', iniciativesController.list)
+routes.get('/users', userController.index)
+
 routes.use(auth)
 
-routes.get('/iniciatives', iniciativesController.list)
 routes.get('/iniciatives/:id', iniciativesController.show)
 routes.post('/iniciatives', iniciativesController.create)
 routes.delete('/iniciatives/:id', iniciativesController.destroy)
 
-routes.get('/users', userController.index)
 routes.get('/users/:id', userController.show)
 routes.put('/users/:id', userController.updatePoints)
 routes.delete('/users/:id', userController.destroy)
